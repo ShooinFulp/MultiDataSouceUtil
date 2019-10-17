26a0976cbecae954bcffabe358ae5d10e7cbb069
1349497941@qq.com <13494947941@qq.com>
2019-10-17 14:17:05 +0800

    初步构建

---
+++ EFMultiDataSouceUtil/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EFMultiDataSouceUtil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            txtState.Text = "";
+            LoopDBMember loop = new LoopDBMember();
+            var dbStrList = loop.LoopMemberDB();
+            PrintState("获取加盟商成功，数量：\n\r" + dbStrList.Count);
+
+            int count = 0;
+            foreach (DictionaryEntry de in dbStrList)
+            {
+                // 调用执行SQL
+                RunSqlCmd(de.Value as string, de.Key as string);
+                count++;
+            }
+            PrintState("执行成功" + count + "个加盟商\n\r");
+        }
+        /// <summary>
+        /// 执行SQL
+        /// </summary>
+        /// <param name="strBSConnection">加盟商连接字符串</param>
+        /// <param name="memberCode">加盟商代码</param>
+        private void RunSqlCmd(string strBSConnection, string memberCode)
+        {
+            SqlConnection connection = new SqlConnection(strBSConnection);
+            connection.Open();
+            PrintState("数据库打开连接成功！\n\r");
+            SqlCommand cmd = new SqlCommand(txtSql.Text, connection);
+            int ffectRows = cmd.ExecuteNonQuery();
+            PrintState("SQL执行成功！\n\r");
+
+            string outPrintStr = string.Format("执行{0}加盟商-连接字符串{1}-影响行数{2}\n\r", memberCode, strBSConnection, ffectRows);
+            PrintState(outPrintStr);
+
+            PrintState(memberCode + "加盟商执行完成！\n\r");
+        }
+
+        /// <summary>
+        /// 输出状态
+        /// </summary>
+        /// <param name="message">状态</param>
+        private void PrintState(string message)
+        {
+            txtState.Text = message + txtState.Text;
+        }
+    }
+
+
+}
---
+++ EFMultiDataSouceUtil/LoopDBMember.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace EFMultiDataSouceUtil
+{
+    class LoopDBMember
+    {
+        public Hashtable LoopMemberDB()
+        {
+            // key, value =》workflowDB,BusinessDB
+            Hashtable memberList = new Hashtable();
+            try
+            {
+                // 获取加盟商API的服务URL
+                string _apiAddress = "http://intpassport.einwin.com";
+                System.Net.HttpWebRequest httpWebRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(_apiAddress + "/ContractService/GetValidServiceMemberInfo");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write("");
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        JavaScriptSerializer jss = new JavaScriptSerializer();
+                        var jsonvalue = jss.Deserialize<ReturnResult<OutValidServiceMemberInfo<OutDatabaseInfo>>>(result);
+                        if (jsonvalue.Data != null)
+                        {
+                            string strWFDB = string.Empty;
+                            string strBSDB = string.Empty;
+                            for (var i = 0; i < jsonvalue.Data.Count; i++)
+                            {
+                                strWFDB = string.Empty;
+                                strBSDB = string.Empty;
+                                var Data = jsonvalue.Data[i];
+                                var DbInfo = Data.DbInfo;
+                                var memberCode = string.Empty;
+                                memberCode = Data.MemberCode;
+                                foreach (var item in DbInfo)
+                                {
+                                    if (item.DbType.Equals("BusinessSystem"))
+                                    {
+                                        strBSDB = string.Format("Server={0};Database={1};Uid={2};Pwd={3};MultipleActiveResultSets=true",
+                                            item.Server, item.Database, item.Username, item.Password);
+
+                                    }
+                                    else if (item.DbType.Equals("Workflow"))
+                                    {
+                                        strWFDB = string.Format("Server={0};Database={1};Uid={2};Pwd={3};MultipleActiveResultSets=true",
+                                                                                  item.Server, item.Database, item.Username, item.Password);
+                                    }
+                                }
+                                if (!string.IsNullOrEmpty(strBSDB))
+                                {
+                                    memberList.Add(memberCode, strBSDB);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex.Message + ex.StackTrace);
+            }
+            return memberList;
+        }
+    }
+
+
+    public class ReturnResult<T>
+    {
+        public IList<T> Data { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public bool Result { get; set; }
+    }
+
+    /// <summary>
+    /// 请求数据库连接接口输出类
+    /// </summary>
+    public class OutDatabaseInfo
+    {
+        /// <summary>
+        /// 服务器地址
+        /// </summary>
+        public string Server { get; set; }
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
+        public string Database { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 数据库配置类型(数据字典项Code)
+        /// </summary>
+        public string DbType { get; set; }
+    }
+
+    /// <summary>
+    /// 获取所有有效服务的盟友信息接口输出类
+    /// </summary>
+    public class OutValidServiceMemberInfo<T>
+    {
+        /// <summary>
+        /// 盟友编号
+        /// </summary>
+        public string MemberCode { get; set; }
+        /// <summary>
+        /// 系统类型
+        /// </summary>
+        public List<EnumSystemType> SystemType { get; set; }
+        /// <summary>
+        /// 数据库连接
+        /// </summary>
+        public List<T> DbInfo { get; set; }
+    }
+
+    /// <summary>
+    /// 系统类型枚举
+    /// </summary>
+    public enum EnumSystemType
+    {
+        PMS = 1,
+        CRM = 2,
+        UMall = 3
+    }
+}
